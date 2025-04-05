9fcff440119beb5bf9b8e801a39bae71a206821d
gaurav <gaurav.rana431@gmail.com>
2025-04-05 17:14:15 +0530

    String Calculator added test negative number and delimiter test case

--- string_calculator.py
+++ string_calculator.py
@@ -1,3 +1,48 @@
+import re
+
 class StringCalculator:
-    def add(self, numbers: str)-> int:
-       pass
+    def __init__(self):
+        self.call_count = 0
+        self.add_occurred = [] 
+
+    def add(self, numbers: str) -> int:
+        self.call_count += 1
+
+        if not numbers:          
+            return 0
+
+        delimiters = [',', '\n']
+        if numbers.startswith('//'):
+            header, numbers = numbers.split('\n', 1)
+            custom_part = header[2:]
+            if custom_part.startswith('['):               
+                delimiters += re.findall(r'\[([^\]]+)\]', custom_part)
+            else:
+                delimiters.append(custom_part)
+
+        delimiter_pattern = '|'.join(map(re.escape, delimiters))
+        num_str = re.split(delimiter_pattern, numbers)
+
+        total = 0
+        negativeNumbers = []
+
+        for num in num_str:
+            if not num:
+                continue
+            value = int(num)
+            if value < 0:
+                negativeNumbers.append(value)
+            elif value <= 1000: 
+                total += value
+
+        if negativeNumbers:
+            raise ValueError(f"negative numbers {', '.join(map(str, negativeNumbers))}")
+
+
+        return total
+
+    def get_called_count(self) -> int:
+        return self.call_count
+
+
+
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -14,6 +14,25 @@ class Testcalculator(unittest.TestCase):
     def test_comma_separated(self):
         self.assertEqual(self.calc.add("1,5"), 6)
 
+    def test_multiple_numbers_comma_separated(self):
+        self.assertEqual(self.calc.add("1,2,3,4,5"), 15)
+
+    def test_newline_as_delimiter(self):
+        self.assertEqual(self.calc.add("1\n2,3"), 6)
+
+    def test_custom_single_char_delimiter(self):
+        self.assertEqual(self.calc.add("//;\n1;2"), 3)
+
+    def test_negative_number(self):
+        with self.assertRaises(ValueError) as context:
+            self.calc.add("1,-2")
+        self.assertEqual(str(context.exception), "negative numbers -2")
+
+    def test_multiple_negative_numbers_message(self):
+        with self.assertRaises(ValueError) as context:
+            self.calc.add("-1,2,-3")
+        self.assertEqual(str(context.exception), "negative numbers -1, -3")
+
 if __name__=="__main__":
     unittest.main()
 
